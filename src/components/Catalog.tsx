4802fc5d075cb08077a297896c6ae29642fb0593
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-01 17:40:14 +0530

    retrive cat catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,8 +1,15 @@
+import axios from 'axios'
 import React, { useEffect } from 'react'
 
 export default function Catalog() {
 
-    
+    useEffect(()=>{
+        const retriveCatBread=async()=>{
+            const response = await axios.get("https://api.thecatapi.com/v1/breeds?limit=20")
+            console.log(response.data);
+        }
+        retriveCatBread()
+    },[])
   return (
     <div>Catalog</div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Catalog from '../components/Catalog'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Catalog/>
+    </div>
   )
 }
