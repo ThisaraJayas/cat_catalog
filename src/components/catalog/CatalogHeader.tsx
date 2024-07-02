2712575ab0d4222dbb0e11e7019b92958bf6c388
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-02 22:54:57 +0530

    design done

--- src/components/catalog/CatalogHeader.tsx
+++ src/components/catalog/CatalogHeader.tsx
@@ -4,8 +4,8 @@ export default function CatalogHeader() {
   return (
     <div>
         <div className="mt-6 flex justify-center">
-        <div>
-          <h1 className="text-3xl font-bold text-center pb-4">Cat Catalog</h1>
+        <div className=''>
+          <h1 className=" bg-blue-700 rounded-full text-white p-4 text-3xl font-bold text-center pb-4">Cat Catalog</h1>
           <div>
             <h2 className="text-lg text-center">
               Explore our collection of cat breeds
